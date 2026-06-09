66681e24e60ac1e1068fbcca6278925359cbd237
David Al-Attar <da380@cam.ac.uk>
2026-06-09 11:53:15 +0100

    updated low rank example

--- work/low_rank.py
+++ work/low_rank.py
@@ -1,28 +1,34 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import pygeoinf as inf
 from pygeoinf.symmetric_space.circle import Sobolev, plot
 
 
 X = Sobolev.from_heat_kernel_prior(0.01, 2.0, 0.1, power_of_two=True)
-print(f"Dimension of space = {X.dim}")
+print(f"Dimension of space    = {X.dim}")
 
 
-A = X.invariant_automorphism(lambda k: 1 / (1 + 0.1 * k**2))
+A = X.invariant_automorphism(lambda k: k * k / (1 + 0.01 * k**4))
 
 mu = X.heat_kernel_gaussian_measure(0.01)
 u = mu.sample()
 
 
-print(f"True trace =  {A.trace}")
-
 B = inf.LowRankEig.from_randomized(
     A, 10, method="variable", power=2, rtol=1e-3, measure=mu
 )
 print(f"Rank of decomposition = {B.rank}")
-print(f"Estimated trace via decomposition = {B.trace}")
 
+
+print(f"Estimated trace = {B.trace}")
+print(f"True trace      = {A.trace}")
 print(
     f"Estimated trace via Hutchinson = {inf.random_trace(A, 10, method="variable", rtol=1e-3)} "
+)
+
+print(f"Covariance norm (shortcut)        = {mu.hilbert_schmidt_norm()}")
+print(
+    f"Covariance norm (random sampling) = {np.sqrt(inf.random_trace(mu.covariance @ mu.covariance, 10, rtol=1e-3))}"
 )
 
 v = A(u)
